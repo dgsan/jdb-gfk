8c85d60e8ac0fe1cfa7c0fd9dccdc227bb8564ac
dgsan <dgsan@server.fake>
2013-05-17 08:59:26 -0700

    Generic relation implemented via post-facto model patching.

--- jdb-gfk.js
+++ jdb-gfk.js
@@ -1,5 +1,6 @@
 var jugglingdb = require('jugglingdb');
 var async = require('async');
+var AbstractClass = jugglingdb.AbstractClass;
 
 function patchAdapter(schema){
     schema.registry = {
@@ -37,7 +38,7 @@ function patchAdapter(schema){
             var self = this;
             var scb = cb;
             var f = {table_name: modelName};
-            self.schema.models.SchemaEntity.findOne(f, function (err, schemaEntity) {
+            this.schema.models.SchemaEntity.findOne(f, function (err, schemaEntity) {
                 if (!err && !schemaEntity) {
                     self.schema.models.SchemaEntity.create({table_name: modelName}, function (err, obj) {
                         if (obj) {
@@ -58,6 +59,7 @@ function patchAdapter(schema){
                     throw "jdb-gfk: Failed to register " + modelName;
                 }
             });
+            this.injectBelongsTo(modelName);
         },
         getEntityById : function (id) {
             return this.schema.models[this.nameByModelDx[id]];
@@ -67,8 +69,106 @@ function patchAdapter(schema){
             this.nameByModelDx[schemaEntity.id] = modelName;
             var m = this.getEntityById(schemaEntity.id);
             m.schemaEntityId = schemaEntity.id;
+        },
+        injectBelongsTo : function (modelName) {
+            this.schema.models[modelName].genericBelongsTo = function(params){
+                var schemaEntityIdColumn = 'schemaEntityId';
+                var relatedIdColumn = 'relatedId';
+                var methodName = 'related';
+                
+                if(params && params.schemaEntityIdColumn){
+                    schemaEntityIdColumn = params.schemaEntityIdColumn;
+                }
+                if(params && params.relatedIdColumn){
+                    relatedIdColumn = params.relatedIdColumn;
+                }
+                if(params && params.as){
+                    methodName = params.as;
+                }
+                
+                this.relations[methodName] = {
+                    type: 'genericBelongsTo',
+                    keyTo: 'id',
+                    schemaTo: schemaEntityIdColumn,
+                    keyFrom: relatedIdColumn,
+                    multiple: false
+                };
+                
+                this.prototype['__finders__'] = this.prototype['__finders__'] || {};
+                
+                this.prototype[methodName] = function (refresh, p) {
+                    if (arguments.length === 1) {
+                        p = refresh;
+                        refresh = false;
+                    } else if (arguments.length > 2) {
+                        throw new Error('Method can\'t be called with more than two arguments');
+                    }
+                    var self = this;
+                    var cachedValue;
+                    if (!refresh && this.__cachedRelations && (typeof this.__cachedRelations[methodName] !== 'undefined')) {
+                        cachedValue = this.__cachedRelations[methodName];
+                    }
+                    if (p instanceof AbstractClass) { // acts as setter
+                        this[schemaEntityIdColumn] = p.constructor.schemaEntityId;
+                        this[relatedIdColumn] = p.id;
+                        this.__cachedRelations[methodName] = p;
+                    } else if (typeof p === 'function') { // acts as async getter
+                        if (typeof cachedValue === 'undefined') {
+                            this.__finders__[methodName].apply(self, 
+                                [
+                                    { 
+                                        schemaEntityId: this[schemaEntityIdColumn], 
+                                        relatedId: this[relatedIdColumn]
+                                    }, 
+                                    function (err, inst) {
+                                        if (!err) {
+                                            self.__cachedRelations[methodName] = inst;
+                                        }
+                                        p(err, inst);
+                                    }
+                                ]
+                            );
+                            return {    
+                                        schemaEntityId: this[schemaEntityIdColumn], 
+                                        relatedId: this[relatedIdColumn]
+                                    };
+                        } else {
+                            p(null, cachedValue);
+                            return cachedValue;
+                        }
+                    } else if (typeof p === 'undefined') { // acts as sync getter
+                        return {schemaEntityId: this[schemaEntityIdColumn], relatedId: this[relatedIdColumn]};
+                    } else { // setter
+                        this[schemaEntityIdColumn] = p.constructor.schemaEntityId;
+                        this[relatedIdColumn] = p.id;
+                        delete this.__cachedRelations[methodName];
+                    }
+                };
+                
+                this.prototype['__finders__'][methodName] = function (relation, cb) {
+                    if (relation === null || !relation.schemaEntityId || !relation.relatedId) {
+                        cb(null, null);
+                        return;
+                    }
+                    var model = this.constructor.schema.registry.getEntityById(relation.schemaEntityId);
+                    if(model){
+                        model.find(relation.relatedId, function (err,inst) {
+                            if (err) return cb(err);
+                            if (!inst) return cb(null, null);
+                            if (inst.id === relation.relatedId) {
+                                cb(null, inst);
+                            } else {
+                                cb(new Error('Permission denied - id mismatch.'));
+                            }
+                        }.bind(this));
+                    } else {
+                        cb(new Error('Model has not been registered for generic use.'));
+                    }
+                };
+            };
         }
     };
+    
 };
 
 
@@ -93,6 +193,7 @@ function prepareSchema(schema, patchAdapter){
     });
     
 };
+
 
 jugglingdb.Schema.prototype.registryInit = function () {
     prepareSchema(this, patchAdapter);
--- test/test.js
+++ test/test.js
@@ -3,20 +3,67 @@ var Schema = require('../jdb-gfk.js').Schema;
 /* @TODO write actual unit tests. */
 
 var jdb = new Schema('memory');
+jdb.registryInit();
 
 jdb.on('jdb-gfk:models_registered', function(){
     console.log(User.schemaEntityId);
     console.log(jdb.models.SchemaEntity.schemaEntityId);
 });
 
-jdb.registryInit();
 var User = jdb.define('User', {
-   email: String,
-   password: String 
+   name: String
 });
 
+var Food = jdb.define('Food', {
+   name: String 
+});
+
+var Like = jdb.define('Like', {
+    q : String,
+    schemaEntityId: Number,
+    relatedId: Number
+})
+
 jdb.registry.registerModels(function(){
+    
+    Like.genericBelongsTo();
+    
     console.log(User.schemaEntityId);
     console.log(jdb.models.SchemaEntity.schemaEntityId);
+    
+    
+    User.create({name: 'Ted'}, function(err, obj){
+        console.log(err || obj);
+        Like.create({q: 'a'}, function(err, lobj){
+            console.log(err || lobj);
+            lobj.related(obj);
+            lobj.save(function(){
+                Like.findOne({where: {q: 'a'}}, function(err, nobj){
+                   console.log(nobj); 
+                   nobj.related(function(err, robj){
+                       console.log(robj);
+                   }); 
+                });
+            });
+            
+        });
+    });
+    
+    Food.create({name: 'Pizza'}, function(err, obj){
+        console.log(err || obj);
+        Like.create({q: 'b'}, function(err, lobj){
+            console.log(err || lobj);
+            lobj.related(obj);
+            lobj.save(function(){
+                Like.findOne({where: {q: 'b'}}, function(err, nobj){
+                   console.log(nobj);
+                   nobj.related(function(err, robj){
+                       console.log(robj);
+                   }); 
+                });
+            });
+        });
+    });
+    
 });
 
